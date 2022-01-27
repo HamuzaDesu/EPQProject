92dca59c7838f0047bbfac48ed4417d4b62ee4cb
Hamza <rafi.hamza2020@gmail.com>
2022-01-27 23:09:39 +0000

    Base login and register finctionality

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
 
+app.secret_key = 'TEMP_SECRET_KEY'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', username = session['user']['username'])
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('/auth/login.html')
+    else:
+        for item in request.form:
+            print(request.form[item])
+
+        session['user'] = {
+            'username': request.form['username']
+        }
+
+        return redirect('/')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'GET':
+        return render_template('/auth/register.html')
+    else:
+        # register account logic here
+        for item in request.form:
+            print(request.form[item])
+        return redirect('/login')
+        
