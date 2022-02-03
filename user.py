ba1c40a621e1c68c90b23240cb2a22b894404360
Hamza <rafi.hamza2020@gmail.com>
2022-02-03 00:56:49 +0000

    cleaning files

--- user.py
+++
@@ -1,7 +0,0 @@
-class User():
-    def __init__(self) -> None:
-        self.isTeacher = False
-        self.userDetails = dict()
-    
-    def authenticate(username, password):
-        pass #logic for authentication here
